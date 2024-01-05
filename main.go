6f2dced9983d52f2544b275fff6fc64d9164fc77
iamsaadMehmood <warriorex.g@gmail.com>
2024-01-05 16:22:38 +0500

    add get and get by id method

--- main.go
+++ main.go
@@ -2,14 +2,38 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
+	products := []map[int]string{{1: "mobile"}, {2: "Laptops"}, {3: "Desktops"}}
+	e.GET("/start", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+	e.GET("/products", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, products)
+	})
+	e.GET("/products/:id", func(c echo.Context) error {
+		var product map[int]string
+		for _, p := range products {
+			for k := range p {
+				pID, err := strconv.Atoi(c.Param("id"))
+				if err != nil {
+					return err
+				}
+				if pID == k {
+					product = p
+				}
+			}
+		}
+		if product == nil {
+			return c.JSON(http.StatusNotFound, "Product not found")
+		}
+		return c.JSON(http.StatusOK, product)
+
+	})
+	e.Logger.Fatal(e.Start(":8080")) //e.Logger.Fatal use for logging error
 }
