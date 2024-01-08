b3ab9687064634c8ec8b0bc7dd3302b30741b821
iamsaadMehmood <warriorex.g@gmail.com>
2024-01-08 17:19:34 +0500

    middleware calling understanding

--- electronics/electronics.go
+++ electronics/electronics.go
@@ -18,10 +18,26 @@ func init() {
 	}
 }
 
+func serverMessage(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		fmt.Println("inside middleware")
+		return next(c)
+	}
+}
+func preServerMessage(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		fmt.Println("inside pre middleware")
+		return next(c)
+	}
+}
+
 // starts the application
 func Start() {
+	e.Use(serverMessage)    // always called after pre can be use for admin validation if needed
+	e.Pre(preServerMessage) //always called first can be use for validate token
 
 	e.POST("/products", createProduct)
+	// e.GET("/products", getProducts, serverMessage) route level middle ware
 	e.GET("/products", getProducts)
 	e.GET("/products/:id", getProduct)
 	e.PUT("/products/:id", updateProduct)
